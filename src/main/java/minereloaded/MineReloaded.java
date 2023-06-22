aea9399027f338f1f654eaa49537f6fff2083fd0
Johannes Pollitt <johannes@pollitt.de>
2023-06-22 21:35:39 +0200

    Created empty spigot plugin.

---
+++ src/main/java/minereloaded/MineReloaded.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package minereloaded;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Copyright (C) 2023 FlorestanII & Elekt0. <br />
+ * All rights reserved.
+ * 
+ * @author FlorestanII, Elekt0
+ * 
+ */
+public class MineReloaded extends JavaPlugin {
+
+}
