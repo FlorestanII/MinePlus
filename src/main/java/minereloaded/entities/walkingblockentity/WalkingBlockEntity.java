899ddea4b2e6e0496dd6aeac5e9d19162ab36716
Johannes Pollitt <johannes@pollitt.de>
2023-10-02 23:42:53 +0200

    Added emmpty base entity classes.

---
+++ src/main/java/minereloaded/entities/walkingblockentity/WalkingBlockEntity.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package minereloaded.entities.walkingblockentity;
+
+import org.bukkit.Location;
+
+/**
+ * Copyright (C) 2023 Johannes Pollitt. <br />
+ * All rights reserved.
+ * 
+ * @author FlorestanII
+ * 
+ */
+public class WalkingBlockEntity {
+
+	/**
+	 * Create a new walking block entity at the given location.
+	 * 
+	 * @param loc the location to spawn the entity at
+	 */
+	public WalkingBlockEntity(Location loc) {
+
+	}
+
+}
---
+++ src/main/java/minereloaded/entities/walkingblockentity/WalkingBlockEntityBase.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package minereloaded.entities.walkingblockentity;
+
+/**
+ * Copyright (C) 2023 Johannes Pollitt. <br />
+ * All rights reserved.
+ * 
+ * @author FlorestanII
+ * 
+ */
+public class WalkingBlockEntityBase {
+
+}
